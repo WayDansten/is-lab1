8c6ff7882483fcf689b6302b83e4694d9dddbb9b
WayDansten <way.dansten@gmail.com>
2025-10-01 18:16:52 +0300

    feat: added websocket for notifications about changed stored objects

--- src/main/java/websocket/WebSocketNotifier.java
+++ src/main/java/websocket/WebSocketNotifier.java
@@ -2,20 +2,18 @@ package websocket;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import dto.LabWorkDTO;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.jms.Session;
 import jakarta.json.Json;
+import jakarta.websocket.Session;
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.server.ServerEndpoint;
 
 @ApplicationScoped
 @ServerEndpoint("/ws")
-public class LabWorkWebSocket {
+public class WebSocketNotifier {
     private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<>());
 
     @OnOpen
@@ -28,10 +26,19 @@ public class LabWorkWebSocket {
         sessions.remove(session);
     }
 
-    public static void broadcast(List<LabWorkDTO> labWorks) {
+    public static void broadcast(WebSocketMessageType type) {
+        String notificationMessage = Json.createObjectBuilder()
+                .add("type", type.name())
+                .build()
+                .toString();
         synchronized (sessions) {
             sessions.forEach(session -> {
-                
+                try {
+                    session.getBasicRemote().sendText(notificationMessage);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 }
